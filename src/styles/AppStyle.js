779bd66b1da9ef2a201648cf28ed7e2a5cfd4204
dajung <kwonkkan@naver.com>
2024-08-29 22:16:11 +0900

    [gitfix] gitignore 수정 후 커밋 초기화

---
+++ src/styles/AppStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const RoutesArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 1400px;
+  padding-left: 30px;
+  margin: auto;
+  padding-right: 30px;
+`;
---
+++ src/styles/HeaderStyle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+export const HeaderContainer = styled.header`
+  background-color: #fc913a;
+  padding: 0 20px 0 20px;
+  width: 100%;
+  height: 50px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  a {
+    color: white;
+    margin-left: 10px;
+    text-decoration: none;
+  }
+`;
+
+export const NavLinks = styled.nav`
+`;
