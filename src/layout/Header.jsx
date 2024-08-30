057d17a99b6de5405277777ab0c8ccd006395917
RYC0208 <ryc0208@naver.com>
2024-08-30 17:47:15 +0900

    [fix] 헤더에서 임의로 로그인/로그아웃 상태 처리

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,19 +1,38 @@
 // src/components/Header.js
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as S from "../styles/HeaderStyle";
 
 const Header = () => {
+  const [user, setUser] = useState(null);
+
+  const handleSignIn = () => {
+    alert("로그인 되었습니다.")
+    setUser("공감해조");
+  }
+  const handleSignOut = () => {
+    alert("로그아웃 처리가 완료됐습니다. 메인페이지로 이동할께요.");
+    setUser(null);
+  };
+
   return (
     <S.HeaderContainer>
       <S.NavLinks>
         <div>
           <Link to="/">어? 그로그램</Link>
         </div>
-        <div>
-          <Link to="/login">로그인</Link>
-          <Link to="/join">회원가입</Link>
-        </div>
+        {user ? (
+          <div>
+            <Link>{user}님 안녕하세요</Link>
+            <Link onClick={handleSignOut}>로그아웃</Link>
+          </div>
+          
+        ) : (
+          <div>
+            <Link onClick={handleSignIn}>로그인</Link>
+            <Link to="/join">회원가입</Link>
+          </div>
+        )}
       </S.NavLinks>
     </S.HeaderContainer>
   );
