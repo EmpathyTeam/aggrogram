a8bf6ceb1794b75882de57d5a2808fe541440f13
RYC0208 <ryc0208@naver.com>
2024-08-30 19:30:33 +0900

    [fix] sidebar,Board 링크 연결 및 스크롤 함수 추가

--- src/components/commons/sideBar.jsx
+++ src/components/commons/sideBar.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as S from "../../styles/sideBarStyle";
 import RoundButton from "./RoundButton";
+import { useNavigate } from "react-router-dom";
+import { AggrogramContext } from "../../context/AggrogramContext";
 const Sidebar = () => {
+  const { user } = useContext(AggrogramContext);
+
+  const navigate = useNavigate("");
+
+  const handleCreatePostClick = () => {
+    if (user === null) {
+      alert("로그인한 유저만 작성이 가능합니다.");
+      navigate("signin");
+    } else {
+      navigate("board");
+    }
+  };
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // 부드러운 스크롤 효과
+    });
+  };
   return (
     <S.SidebarContainer>
-      <RoundButton type="write">글 작성</RoundButton>
-      <RoundButton type="top">TOP</RoundButton>
+      <RoundButton onClick={handleCreatePostClick} type="write">
+        글 작성
+      </RoundButton>
+      <RoundButton onClick={scrollToTop} type="top">TOP</RoundButton>
     </S.SidebarContainer>
   );
 };
