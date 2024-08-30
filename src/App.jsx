96ab3e081f31faf0479fc71a961f44ad7099e6aa
RYC0208 <ryc0208@naver.com>
2024-08-30 15:26:26 +0900

    [add] shared폴더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main";
-import Join from "./pages/Join";
-import MyPage from "./pages/MyPage";
-import Board from "./pages/Board";
-import * as S from "./styles/AppStyle";
-import LayOut from "./components/LayOut";
-
+import React from "react";
+import Router from "./shared/Router";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <S.RoutesArea>
-          <Routes>
-            <Route path={"/"} element={<LayOut />}>
-              <Route index element={<Main />} />
-              <Route path={"join"} element={<Join />} />
-              <Route path={"myPage"} element={<MyPage />} />
-              <Route path={"board"} element={<Board />} />
-            </Route>
-          </Routes>
-        </S.RoutesArea>
-      </BrowserRouter>
+      <Router />
     </>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,53 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import React, { useEffect, useState } from "react";
+import * as S from "../styles/MainStyle";
+import { createClient } from "@supabase/supabase-js";
+
+const API_URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(API_URL, API_KEY);
+
+
 const Main = () => {
-  return <div>main</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  console.log("posts =>", posts);
+
+  const getPosts = async () => {
+    const { data } = await supabase.from("posts").select();
+    setPosts(data);
+    console.log("data =>", data);
+  };  
+
+
+  return (
+    <S.MainContainer>
+      <S.PostList>
+        {posts.map((card) => (
+          <S.PostCard key={card.id}>
+            <div className="imageArea">
+              <img src={card.img_url} alt="이미지" />
+            </div>
+            <div className="contentsArea">
+              <h1>{card.title}</h1>
+              <div className="profileArea">
+                <div className="userInfo">
+                  <img src="" alt=" " />
+                  <p>{card.nickname}</p>
+                </div>
+                <div className="engagementStats">
+                  <p>좋아요</p>
+                  <p>조회수</p>
+                </div>
+              </div>
+            </div>
+          </S.PostCard>
+        ))}
+      </S.PostList>
+    </S.MainContainer>
+  );
 };
 
 export default Main;
