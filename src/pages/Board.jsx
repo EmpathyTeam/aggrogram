78efb780c5b00d4bf8f0f23c653938ceb91a3fe2
junjun932 <1004ji93@gmail.com>
2024-08-31 23:35:48 +0900

    [update 뒤로가기 버튼 연결]

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,6 +1,11 @@
+import * as S from "../styles/BoardStyle";
+
 import React, { useEffect, useState } from "react";
+
 import AddBoard from "../components/board/AddBoard.jsx";
+import styled from "styled-components";
 import { useAggrogram } from "../contexts/AggrogramContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Board = () => {
   // const searchParams = new URLSearchParams(location.search);
@@ -8,17 +13,36 @@ const Board = () => {
   const { posts } = useAggrogram();
   const foundPost = posts.find((p) => p.id === 1000);
   console.log(foundPost);
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate("/");
+  };
   return (
     <div>
       {!foundPost ? (
         <div>게시글 찾을 수 없음</div>
       ) : (
         <>
+          <S.ContentWrapper>
+            <S.BoxContainer>
+              <div>
+                <div>
+                  <div> {foundPost.title}</div>
+                  <div> {foundPost.created_at}</div>
+
+                  <S.ImageBox>사진</S.ImageBox>
+                  <S.MainContainer>{foundPost.context}</S.MainContainer>
+                </div>
+              </div>
+            </S.BoxContainer>
+          </S.ContentWrapper>
           <div>
-            {foundPost.title}
-            {foundPost.created_at}
-            {foundPost.context}
-            {foundPost.nickname}
+            <S.ButtonWrapper>
+              <button onClick={handleBack}>뒤로가기</button>
+
+              <button> 수정하기</button>
+              <button>삭제하기</button>
+            </S.ButtonWrapper>
           </div>
         </>
       )}
---
+++ src/styles/BoardStyle.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  /* height: 100vh; */
+`;
+
+export const BoxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: grey;
+  width: 80%;
+  margin-top: 50px;
+  padding: 50px;
+  border-radius: 20px;
+  text-align: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 20px;
+  gap: 300px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+`;
+
+export const ImageBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: red;
+  width: 90%;
+  margin-top: 50px;
+  padding: 50px;
+  border-radius: 20px;
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: green;
+  width: 90%;
+  margin-top: 50px;
+  padding: 50px;
+  border-radius: 20px;
+`;
