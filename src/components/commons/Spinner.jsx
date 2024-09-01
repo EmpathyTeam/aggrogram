1fbba0a624ab0699a659fccf5030e69a91f72a7d
RYC0208 <ryc0208@naver.com>
2024-09-02 00:32:08 +0900

    [feat] 스피너 컴포넌트 추가

---
+++ src/components/commons/Spinner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledSpinner = styled.div`
+  border: 8px solid rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  border-top: 8px solid #fc913a;
+  width: 50px;
+  height: 50px;
+  animation: ${spin} 1s linear infinite;
+  margin: 0 auto;
+  display: block;
+`;
+
+const Spinner = () => {
+  return <StyledSpinner />;
+};
+
+export default Spinner;
+오전 12:17 2024-09-02
