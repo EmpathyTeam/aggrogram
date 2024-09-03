acd379449bcf86cd453e627592b7a9767a5cb614
dajung <kwonkkan@naver.com>
2024-09-03 11:36:41 +0900

    [fix] 회원가입 시 alert 문구 수정

--- src/components/Join/SignUp.jsx
+++ src/components/Join/SignUp.jsx
@@ -66,7 +66,7 @@ const SignUp = () => {
         }
       }
     });
-    alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+    alert(`${nickname}님 환영합니다! 지금 바로 어?그로그램을 즐겨보세요.😎`);
     navigate("/signin");
   };
 
